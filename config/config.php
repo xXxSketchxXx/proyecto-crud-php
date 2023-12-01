da2e3d04c1a85e44eac8ed44f7ff562a5390da29
Sketch <davidaqm2011@gmail.com>
2023-12-01 09:14:29 +0100

    Estructura basica

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'ecommerce_informatica';
+    const DB_USER = 'admin_ecommerce';
+    const DB_PASSWORD = 'P4ssw0rd';
+    const DB_CHARSET = 'utf8';
+?>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ordenadores Marilu</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <h1>
+                <a href="#">Tienda</a>
+            </h1>
+        </nav>
+    </header>
+
+    <section>
+        <div class="container">
+            <h2>
+                Listado de productos
+            </h2>
+            <div class="btn_add_product">
+                <a href="#">+ Producto</a>
+            </div>
+        </div>
+        <div class="container_cards">
+            <div class="card">
+                <img src="https://picsum.photos/200/300" alt="">
+                <p>Ordenador 1</p>
+                <p>1000€</p>
+                <div class="btn_edit">
+                    <a href="#">Editar</a>
+                </div>
+                <div class="btn_delete">
+                    <a href="#">Eliminar</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
