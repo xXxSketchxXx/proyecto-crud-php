8946e3c58e3450f997a363fb92cef3e40a8d7b4e
Sketch <davidaqm2011@gmail.com>
2023-12-01 10:55:07 +0100

    Creacion directorio BD y script create-db.sql

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
     const DB_HOST = 'localhost';
-    const DB_NAME = 'ecommerce_informatica';
+    const DB_NAME = 'ecommerce';
     const DB_USER = 'admin_ecommerce';
     const DB_PASSWORD = 'P4ssw0rd';
     const DB_CHARSET = 'utf8';
